d2f7b29a20d2fd1234f356c37e873f6293c71644
studyshubham <studyshubham007@gmail.com>
2022-12-13 07:38:13 +0530

    two actions by single reducers

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const redux = require('redux')
 const createStore = redux.createStore 
 
 const BUY_CAKE = 'BUY_CAKE'
+const BUY_ICECREM = 'BUY_ICECREM'
 
 function buyCake(){
     return {
@@ -10,10 +11,17 @@ function buyCake(){
     }
 }
 
+function buyIceCreams(){
+    return {
+        type: BUY_ICECREM
+    }
+}
+
 // (previousState, action) => newState
 
 const initialState = {
-    numOfCakes: 10
+    numOfCakes: 10,
+    numOfIceCreams: 20
 }
 
 const reducer = (state = initialState, action) => {
@@ -21,6 +29,11 @@ const reducer = (state = initialState, action) => {
         case BUY_CAKE: return {
             ...state,
             numOfCakes: state.numOfCakes - 1
+        }
+
+        case BUY_ICECREM: return {
+            ...state,
+            numOfIceCreams: state.numOfIceCreams - 1
         }
         default: return state
     }
@@ -32,4 +45,8 @@ const unsubscribe = store.subscribe(() => console.log('Updated state', store.get
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
+
+store.dispatch(buyIceCreams())
+store.dispatch(buyIceCreams())
+store.dispatch(buyIceCreams())
 unsubscribe()
